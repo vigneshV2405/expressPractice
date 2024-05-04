cf6cc5f676d1014b631a731e2c2b915fb63d5047
vigneshV2405 <vigneshvarikolu133@gmail.com>
2024-05-04 13:49:50 +0530

    ecommerce basic

--- abc.js
+++ abc.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const app = express()
 
+app.set('view-engine','ejs')
+app.use(express.static("staticfiles"))
 app.get('/products',(req,res)=>{
     fetch('https://fakestoreapi.com/products')
     .then((res)=>{return res.json()})
     .then((data)=>{
-        res.send(data)
+        res.render("productslist.ejs",{products:data})
+        //res.send(data)
+    })
+})
+
+app.get('/search',(req,res)=>{
+    console.log(req.query)
+    res.send('wait...............')
+})
+app.get('/product/:id',(req,res)=>{
+    fetch("https://fakestoreapi.com/products/"+req.params.id)
+    .then((res)=>{return res.json()})
+    .then((data)=>{
+        res.render("product.ejs",{product:data})
+        //res.send(data)
     })
 })
 
